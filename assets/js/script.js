d928ac2b1907fa4bba6050865d2bd92ba5142a0f
elgalloloco <zreggert@gmail.com>
2021-03-09 17:46:28 -0500

    created functions in js to get the questions and answers to display to the DOM with the answers being in seperate buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,58 @@
 var startButton = document.querySelector(".start");
 var timer = document.querySelector(".timer");
 var mainContainer = document.querySelector("main");
+var questionContainerEL = document.getElementById("questions-container");
+var answerButtonsEl = document.getElementById("answer-buttons");
+var questionEl = document.getElementById("question");
 
+var currentQuestionIndex;
 
 var timerId;
 var counter = 75;
+
+var quizQuestions = [
+    {
+        question: "Commonly used data types do not inlude ______.",
+        answers: [
+            {text: "alerts", correct: true},
+            {text: "boolean", correct: false},
+            {text: "numbers", correct: false},
+            {text: "strings", correct: false},
+        ]
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        answers: [
+            {text: "quotes", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "parentheses", correct: true},
+            {text: "square brackets", correct: false},
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: 3
+    },
+    {
+        question: "String values must be enclosed within ______",
+        answers: [
+            {text: "commas", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "quotes", correct: true},
+            {text: "parentheses", correct: false},
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is ______",
+        answers: [
+            {text: "JavaScript", correct: false},
+            {text: "console.log", correct: true},
+            {text: "terminal/bash", correct: false},
+            {text: "for loops", correct: false},
+        ]
+    },
+];
 
 
 timer.textContent = counter;
@@ -12,12 +60,28 @@ timer.textContent = counter;
 startButton.addEventListener("click", function(event) {
     //console.log("button clicked");
     startTimer();
-    displayQuestion();
+    startGame();
 })
 
-function displayQuestion() {
-    mainContainer.innerHTML = "";
-    mainContainer.innerHTML
+function startGame() {
+    mainContainer.classList.add("hide");
+    questionContainerEL.classList.remove("hide");
+    currentQuestionIndex = 0;
+    nextQuestion();
+}
+
+function nextQuestion() {
+    displayQuestion(quizQuestions[currentQuestionIndex]);
+}
+
+function displayQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        answerButtonsEl.appendChild(button);
+    })
+    
 }
 
 function startTimer() {
@@ -29,34 +93,7 @@ function startTimer() {
     }, 1000)
 }
 
-function stopTimer () {
+function stopTimer() {
     clearInterval(timerId);
 }
 
-var quizQuestions = [
-    {
-        question: "Commonly used data types do not inlude ______.",
-        choices: ["alerts", "boolean", "numbers", "strings"],
-        answer: 0
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within ______.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: 2
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ______.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: 3
-    },
-    {
-        question: "String values must be enclosed within ______",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: 2
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is ______",
-        choices: ["JavaScript", "console.log", "terminal/bash", "for loops"],
-        answer: 1
-    },
-];
