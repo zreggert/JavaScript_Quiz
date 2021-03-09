885aba12415b97e1144677c43be9e36114916d1e
elgalloloco <zreggert@gmail.com>
2021-03-08 22:18:03 -0500

    cleaned up some css and added the questions to the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var startButton = document.querySelector(".start");
 var timer = document.querySelector(".timer");
 var mainContainer = document.querySelector("main");
-var timerId;
-var counter = 75
 
+
+var timerId;
+var counter = 75;
 
 
 timer.textContent = counter;
@@ -24,10 +25,38 @@ function startTimer() {
         counter--;
         timer.textContent = counter;
 
-        console.log(counter);
+        console.log(counter); 
     }, 1000)
 }
 
 function stopTimer () {
     clearInterval(timerId);
 }
+
+var quizQuestions = [
+    {
+        question: "Commonly used data types do not inlude ______.",
+        choices: ["alerts", "boolean", "numbers", "strings"],
+        answer: 0
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: 3
+    },
+    {
+        question: "String values must be enclosed within ______",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is ______",
+        choices: ["JavaScript", "console.log", "terminal/bash", "for loops"],
+        answer: 1
+    },
+];
